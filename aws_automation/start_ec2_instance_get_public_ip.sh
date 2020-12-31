f59e55d02612128d4a76a518a73700ad1a1a977c
iamjjanga-ouo <namils147@kakao.com>
2020-12-31 14:36:50 +0900

    add TO DO

--- aws_automation/start_ec2_instance_get_public_ip.sh
+++ aws_automation/start_ec2_instance_get_public_ip.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# TO DO
+# 1. get instance-id using tags
+
 # start instance
 aws ec2 start-instances --instance-ids i-08371cbe3f1d54504 --no-cli-pager
 # wait instance state is 'running'
@@ -9,7 +12,6 @@ new_ip=$(aws ec2 describe-instances --instance-ids i-08371cbe3f1d54504 \
     --query 'Reservations[*].Instances[*].PublicIpAddress' \
     --output text --no-cli-pager)
 echo $new_ip
+
 # replace ip address in SSH config(~/.ssh/config)
 #sed -i -e "s|\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b|${newip}|g" ~/.ssh/config
-# Connect shell_ec2
-ssh -i "~/.ssh/seoul.pem" ubuntu@$new_ip
