9bee52e07d3bedf43439a1193b6293a88ec1ad04
iamjjanga-ouo <namils147@kakao.com>
2021-01-01 09:55:35 +0900

    Add tags key, value by User input

--- aws_automation/start_ec2_instance_get_public_ip.sh
+++ aws_automation/start_ec2_instance_get_public_ip.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
-# TO DO
-# 1. get instance-id using tags
+# Read user tags
+read -p "Tag key : " tag_key
+read -p "Tag Value : " tag_value
 
 # Get tags { Name=Shell } instance id
-instance_id=$(aws ec2 describe-instances --filter="Name=tag:Name,Values=Shell" \
+instance_id=$(aws ec2 describe-instances --filter="Name=tag:$tag_key,Values=$tag_value" \
   --query "Reservations[].Instances[].InstanceId" --output=text --no-cli-pager)
 
 # start instance
@@ -16,6 +17,3 @@ new_ip=$(aws ec2 describe-instances --instance-ids $instance_id \
     --query 'Reservations[*].Instances[*].PublicIpAddress' \
     --output text --no-cli-pager)
 echo $new_ip
-
-# replace ip address in SSH config(~/.ssh/config)
-#sed -i -e "s|\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b|${newip}|g" ~/.ssh/config
